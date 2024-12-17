5c552ecd9abf17fb79596dd538cf51d89a007831
Balasuriya <111849019+Balasuriyabala@users.noreply.github.com>
2024-12-17 13:51:44 +0530

    Delete Terraform directory

--- Terraform/Day 4/multiple tfvars/dev.tfvars
+++
@@ -1,2 +0,0 @@
-aws_instance_type = "t2.medium"
-aws_instance_count = 2
--- Terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  description = "VPC CIDR RANGE"
-  type = string
-}  
-
-variable "subnet_cidr" {
-    description = "Subnet CIDRS"  
-    type = list(string)
-}
